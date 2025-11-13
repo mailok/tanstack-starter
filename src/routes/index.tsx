c1b9da724bad9ec7cde099499355f256016208e3
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-11-13 19:49:02 -0300

    add shadcn ui components and utility hooks; update package dependencies

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
-
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  return <>hola mmundo</>
+  return (
+    <>
+      hola mmundo <Button>Click me</Button>
+    </>
+  )
 }
