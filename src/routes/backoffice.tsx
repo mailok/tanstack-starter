4b03d1bf5d5fff7e530395f1824e07dadb2fe40d
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-11-13 22:19:38 -0300

    implement sidebar layout in backoffice route with AppSidebar, header, and GitHub button; integrate SidebarProvider and SidebarInset components

--- src/routes/backoffice.tsx
+++ src/routes/backoffice.tsx
@@ -1,4 +1,12 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { AppSidebar } from '@/components/app-sidebar'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar'
 
 export const Route = createFileRoute('/backoffice')({
   component: RouteComponent,
@@ -6,8 +14,41 @@ export const Route = createFileRoute('/backoffice')({
 
 function RouteComponent() {
   return (
-    <div>
-      backoffice Layout <Outlet />
-    </div>
+    <SidebarProvider>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+          <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mx-2 data-[orientation=vertical]:h-4"
+            />
+            {/* <Breadcrumbs /> */}
+            <div className="ml-auto flex items-center gap-2">
+              <Button
+                variant="ghost"
+                asChild
+                size="sm"
+                className="hidden sm:flex"
+              >
+                <a
+                  href="https://github.com/mailok/react-spa-starter"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="dark:text-foreground"
+                >
+                  GitHub
+                </a>
+              </Button>
+              {/* <ModeToggle /> */}
+            </div>
+          </div>
+        </header>
+        <div className="size-full">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
