fd7359057ae2011c9ebd7b66098d7bc1f21ba5df
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-11-17 01:22:57 -0300

    Refactor index route to remove environment loading and simplify App component; retain UnderConstruction display for the home page.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,10 @@
 import { UnderConstruction } from '@/components/under-construction'
 import { createFileRoute } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
-
-const getEnv = createServerFn({ method: 'GET' }).handler(async () => {
-  return {
-    DATABASE_URL: process.env.DATABASE_URL,
-  }
-})
 
 export const Route = createFileRoute('/')({
   component: App,
-  loader: async () => {
-    return {
-      env: await getEnv(),
-    }
-  },
 })
 
 function App() {
-  const { env } = Route.useLoaderData()
-  return (
-    <>
-      <p>ENV: {env.DATABASE_URL}</p>
-      <UnderConstruction pageName="Home" />
-    </>
-  )
+  return <UnderConstruction pageName="Home" />
 }
