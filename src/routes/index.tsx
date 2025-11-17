f5c352c10c42df08cab8a472db11e25b71805184
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-11-17 01:12:23 -0300

    testing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,28 @@
 import { UnderConstruction } from '@/components/under-construction'
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
+
+const getEnv = createServerFn({ method: 'GET' }).handler(async () => {
+  return {
+    DATABASE_URL: process.env.DATABASE_URL,
+  }
+})
 
 export const Route = createFileRoute('/')({
   component: App,
+  loader: async () => {
+    return {
+      env: await getEnv(),
+    }
+  },
 })
 
 function App() {
-  return <UnderConstruction pageName="Home" />
+  const { env } = Route.useLoaderData()
+  return (
+    <>
+      <p>ENV: {env.DATABASE_URL}</p>
+      <UnderConstruction pageName="Home" />
+    </>
+  )
 }
