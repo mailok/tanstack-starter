fe8b3bd58a428cc539aad526e0ce00c7d6ad78c1
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-11-21 14:35:28 -0300

    feat: Configure pnpm workspace, integrate TanStack Router/Query and React Hooks ESLint plugins, and update dependencies.

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,16 @@
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import reactHooks from 'eslint-plugin-react-hooks'
+import pluginRouter from '@tanstack/eslint-plugin-router'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+  ...pluginRouter.configs['flat/recommended'],
+  ...pluginQuery.configs['flat/recommended'],
+ reactHooks.configs.flat.recommended,
+  {
+    ignores: ['.output/**', 'dist/**', 'node_modules/**'],
+  },
+]
