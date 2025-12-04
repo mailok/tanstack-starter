3ad1655e3047e03fe8aada3fec5fdd29d943d603
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-12-04 13:07:41 -0300

    feat: Enhance QueryClient setup with mutation caching and custom metadata for success/error handling

--- src/integrations/tanstack-query/root-provider.tsx
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -1,7 +1,47 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  MutationCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+declare module '@tanstack/react-query' {
+  interface Register {
+    mutationMeta: {
+      invalidateQueries?: string[]
+      successMessage?: string
+      errorMessage?: string
+    }
+  }
+}
 
 export function getContext() {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: import.meta.env.PROD,
+        /* staleTime: 10 * 1000 */ // 10s
+      },
+    },
+    mutationCache: new MutationCache({
+      onSuccess: (_data, _variables, _context, mutation) => {
+        if (mutation.meta?.successMessage) {
+          // TODO: notify success message
+        }
+      },
+      onError: (_error, _variables, _context, mutation) => {
+        if (mutation.meta?.errorMessage) {
+          // TODO: notify error message
+        }
+      },
+      onSettled: (_data, _error, _variables, _context, mutation) => {
+        if (mutation.meta?.invalidateQueries) {
+          queryClient.invalidateQueries({
+            queryKey: mutation.meta.invalidateQueries,
+          })
+        }
+      },
+    }),
+  })
   return {
     queryClient,
   }
